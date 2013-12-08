7e7983da82492724f58aafe7a6a55a330fab8f65
Jon Dawson <chips@jondawson.org.uk>
2013-12-08 18:52:40 +0000

    remove c generation of adder

--- adder/file_reader_b.c
+++
@@ -1,7 +0,0 @@
-void file_reader_b(){
-	unsigned temp;
-	while(1){
-		temp = file_read("stim_b");
-		output_z(temp);
-	}
-}
--- adder/file_writer.c
+++
@@ -1,5 +0,0 @@
-void file_writer(){
-	while(1){
-		file_write(input_a(), "resp_z");
-	}
-}
